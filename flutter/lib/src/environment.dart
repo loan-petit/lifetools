6520e8d7ced5c83bc9225c0b63f9d231e0c6c050
loan-petit <petit.loan1@gmail.com>
2020-03-05 15:26:48 +0100

    fix (flutter): Use HTTPS Prisma endpoint instead of HTTP to avoid Mixed Content error.

--- flutter/lib/src/environment.dart
+++ flutter/lib/src/environment.dart
@@ -21,7 +21,7 @@ class Environment {
   static void prod() {
     env = EnvironmentType.PROD;
     vars = {
-      'apiEndpoint': 'http://35.180.173.108:8081/',
+      'apiEndpoint': 'https://lifetools.loanpetit.com:8081/',
     };
   }
 }
